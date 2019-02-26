41a7fcdab88f2c61d9f1ac31326040f4fbad92de
weishian-wang <weishianwang@gmail.com>
2019-02-26 15:08:22 -0500

    Set headers for CORS

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const app = express();
 
 app.use(bodyParser.json()); // application/json
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-hEADERS', 'Content-Type, Authorization');
+  next();
+});
+
 app.use('/feed', feedRoutes);
 
 app.listen(PORT, console.log(`Server is listening on port ${PORT}`));
